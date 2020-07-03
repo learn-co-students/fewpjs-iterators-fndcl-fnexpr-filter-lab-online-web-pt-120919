798c6250505c6ab1235d537bf2875d9718b47a47
github username <github email address>
2020-07-03 10:33:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(collection, string) {
+    return collection.filter(s => s.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(collection, string) {
+    return collection.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+function matchName(collection, string) {
+    return collection.filter(s => s.name === string)
+}
