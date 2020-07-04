a1199a1b972b745ab67a77595adb05776661c5fe
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-04 15:14:43 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, str){
+return arr.filter(driver => 
+  driver.toLowerCase() === str.toLowerCase()
+)}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
