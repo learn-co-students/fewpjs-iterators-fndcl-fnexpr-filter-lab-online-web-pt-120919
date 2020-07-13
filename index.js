1e5d5e2123deda5494c3a1ac6201c2af24030baa
Sarah Condon <s.j.condon01@gmail.com>
2020-07-12 20:53:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(e => {
+        return e.toLowerCase() == string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(str => {
+        return str[0].toLowerCase() == string[0].toLowerCase()
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(str => {
+        return str.name.toLowerCase() == string.toLowerCase()
+    })
+}
