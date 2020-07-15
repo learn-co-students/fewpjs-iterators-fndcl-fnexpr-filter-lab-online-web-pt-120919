052c68116bc791a1e4121f663804be1a1e043c72
Justin <justinms@outlook.com>
2020-07-14 20:18:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(function(name) {
+        return name.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(function(name) {
+        return name.startsWith(string);
+    })
+}
+
+function matchName(array, string){
+    return array.filter(function(object) {
+        return object["name"] === string
+    })
+}
