f4e56a3b4b194ad5d67fc5d617d37351000b71b8
hnnav <henna.v@icloud.com>
2020-07-17 16:19:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching(drivers, lookingFor) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === lookingFor.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(drivers, lookingFor) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(lookingFor.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(drivers, lookingFor) {
+    return drivers.filter( record => record.name === lookingFor)
+  }
