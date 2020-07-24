8ac51ad81946fab206a3f802c5f8dc04404fda71
Matthew <mattcorralejo1@gmail.com>
2020-07-24 01:20:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, string){
+    let match = drivers.filter(n => {return n.toLowerCase() === string.toLowerCase()})
+    return match
+}
+
+function matchName(drivers, string){
+let search = drivers.filter(n => {return n.name === string}) //someting to do with changing case
+
+return search
+}
+
+function fuzzyMatch(drivers, string){
+let fuz = drivers.filter(n => {return n.startsWith(string)})
+return fuz //return to this after learning more about using fuzzy
+
+}
