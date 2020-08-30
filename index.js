342b5c0584bafc78f838b8927c49271ac2633336
seung_kim <seunglives@hotmail.com>
2020-08-30 06:11:24 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// Code your solution he
+
+function findMatching(drivers, name) {
+    return drivers.filter(match => 
+        match.toLowerCase() === name.toLowerCase()
+        )
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(match => 
+        match.toLowerCase().indexOf(name.toLowerCase()) === 0
+        )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(match => match.name === name)
+
+}
