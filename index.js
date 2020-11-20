2b09eecfc99e446e86204c904eae8bf5f3c35d44
Candice Peters <51180851+cep104@users.noreply.github.com>
2020-11-19 22:31:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ function fuzzyMatch(source, testString) {
       possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
     )
   }
+// "sammy".indexOf("sa") === 0 it will equal zero till it doesn't match anymore then it will equal -1
+// "sammy".indexOf("sae") === -1
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => {
+        return driver.startsWith(string)
+    });
+}
+
+
 
   function matchName(source, nameDriver) {
     return source.filter( driver => driver.name === nameDriver)
